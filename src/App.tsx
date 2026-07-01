8dbb040f8c5c110db9032ddd5016d86d7eb9435c
Korhan Deniz Akın <korhandenizakin@gmail.com>
2026-07-01 19:29:46 +0200

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const yRowKeep = yDoc.getMap("row-keep");
 //------------------------------------------------------------------------
 
 const wsProvider = new WebsocketProvider(
-  "ws://localhost:1236",
+  "ws://localhost:1237",
   "my-roomname",
   yDoc,
 );
